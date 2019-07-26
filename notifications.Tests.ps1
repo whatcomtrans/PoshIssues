356c008d3dbabb6f1ff15c2842cdc16418505362
Josh Nylander <joshn@ridewta.com>
2019-07-25 20:33:08 -0700

    cleaned up tests

--- notifications.Tests.ps1
+++ notifications.Tests.ps1
@@ -5,12 +5,12 @@ Import-Module .\PoshIssues -Force
 
 describe "Send-IssueMailMessage" {
     $fixes = @()
-    $fixes += New-IssueFix -FixCommand {echo "Hello Completed"} -FixDescription "Completed fix" -CheckName "Greetings" -Status Complete -NotificationCount 1
-    $fixes += New-IssueFix -FixCommand {echo "Hello Pending 1"} -FixDescription "Pending fix 1" -CheckName "Greetings" -Status Pending -NotificationCount 1
-    $fixes += New-IssueFix -FixCommand {echo "Hello Pending 2"} -FixDescription "Pending fix 2" -CheckName "Greetings" -Status Pending
-    $fixes += New-IssueFix -FixCommand {echo "Hello Error"} -FixDescription "Error fix" -CheckName "Greetings" -Status Error -NotificationCount 1
-    $fixes += New-IssueFix -FixCommand {echo "Hello Canceled"} -FixDescription "Canceled fix" -CheckName "Greetings" -Status Canceled
-    $fixes += New-IssueFix -FixCommand {echo "Hello Held"} -FixDescription "Held fix" -CheckName "Greetings" -Status Hold -NotificationCount 1
+    $fixes += New-IssueFix -FixCommand {Write-Output "Hello Completed"} -FixDescription "Completed fix" -CheckName "Greetings" -Status Complete -NotificationCount 1
+    $fixes += New-IssueFix -FixCommand {Write-Output "Hello Pending 1"} -FixDescription "Pending fix 1" -CheckName "Greetings" -Status Pending -NotificationCount 1
+    $fixes += New-IssueFix -FixCommand {Write-Output "Hello Pending 2"} -FixDescription "Pending fix 2" -CheckName "Greetings" -Status Pending
+    $fixes += New-IssueFix -FixCommand {Write-Output "Hello Error"} -FixDescription "Error fix" -CheckName "Greetings" -Status Error -NotificationCount 1
+    $fixes += New-IssueFix -FixCommand {Write-Output "Hello Canceled"} -FixDescription "Canceled fix" -CheckName "Greetings" -Status Canceled
+    $fixes += New-IssueFix -FixCommand {Write-Output "Hello Held"} -FixDescription "Held fix" -CheckName "Greetings" -Status Hold -NotificationCount 1
 
     it "Message should be sent and 6 fixes returned" {
         $results = $fixes | Send-IssueMailMessage
@@ -24,22 +24,22 @@ describe "Send-IssueMailMessage" {
         ($results | Where-Object NotificationCount -gt 0).Count | Should be 2
     }
 
-    $heldFix = New-IssueFix -FixCommand {echo "Hello Hold"} -FixDescription "Held fix" -CheckName "Greetings" -Status Hold -NotificationCount 2
+    $heldFix = New-IssueFix -FixCommand {Write-Output "Hello Hold"} -FixDescription "Held fix" -CheckName "Greetings" -Status Hold -NotificationCount 2
     it "Message should NOT be sent as it is held and 0 fixes returned" {
         $results = $heldFix | Send-IssueMailMessage -ReturnOnlySent
         ($results | Measure-Object).Count | Should be 0
     }
 
     $heldFixes = @()
-    $heldFixes += New-IssueFix -FixCommand {echo "Hello Hold 1"} -FixDescription "Held fix 1" -CheckName "Greetings" -Status Hold -NotificationCount 2
-    $heldFixes += New-IssueFix -FixCommand {echo "Hello Hold 2"} -FixDescription "Held fix 2" -CheckName "Greetings" -Status Hold -NotificationCount 2
+    $heldFixes += New-IssueFix -FixCommand {Write-Output "Hello Hold 1"} -FixDescription "Held fix 1" -CheckName "Greetings" -Status Hold -NotificationCount 2
+    $heldFixes += New-IssueFix -FixCommand {Write-Output "Hello Hold 2"} -FixDescription "Held fix 2" -CheckName "Greetings" -Status Hold -NotificationCount 2
     it "Messages should NOT be sent as they are held and 0 fixes returned" {
         $results = $heldFixes | Send-IssueMailMessage -ReturnOnlySent
         ($results | Measure-Object).Count | Should be 0
     }
 
     $fixes = @()
-    $fixes += New-IssueFix -FixCommand {echo "Hello Completed"} -FixDescription "Completed fix" -CheckName "Greetings" -Status Complete -NotificationCount 1
+    $fixes += New-IssueFix -FixCommand {Write-Output "Hello Completed"} -FixDescription "Completed fix" -CheckName "Greetings" -Status Complete -NotificationCount 1
     $fixes += $heldFix
     it "Message should be sent and 2 fix returned" {
         $results = $fixes | Send-IssueMailMessage
